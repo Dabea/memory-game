48c7ac5ae8ec9bd64da0bcb57a26082be3b73c0b
Abraham <abeis@msn.com>
2018-06-25 23:14:29 -0500

    added shffule of images by array

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import Card from "./componets/Card/Card";
 
+const cardList = ["1","2","3", "4" ,"5", "6", "7" ,"8", "9", "10"];
+
 class App extends Component {
  
   cardAlert = (message) => {
     alert(message);
   }
+
+  randomizeCards = () => {
+    var shuffled = cardList.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
+    return shuffled.map((number) => <Card color="blue" cardCount={number}  handleClick={this.cardAlert}  > </Card>)
+  }
+
+  
+ 
 
   render() {
     return (
@@ -17,8 +27,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div className="card-grid"> 
-          <Card color="blue" cardCount="1"  handleClick={this.cardAlert}  > </Card>
-          <Card color="green" cardCount="2"  handleClick={this.cardAlert}  > </Card>
+           {this.randomizeCards()}
+          
        </div>
       </div>
     );
